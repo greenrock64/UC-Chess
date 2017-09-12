837f4c4b0345224f84e3fddc606b4e2a142453bb
Greenrock64 <greenrock64@users.noreply.github.com>
2017-09-12 14:41:48 +1000

    First Commit

---
+++ UC-Chess/Chess.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 1 - Pawn
+ * 2 - Castle
+ * 3 - Bishop
+ * 4 - Knight
+ * 5 - Queen
+ * 6 - King
+ */ 
+
+namespace UC_Chess
+{
+    class Chess
+    {
+        int[,] board;
+        public Chess()
+        {
+            board = new int[8, 8];
+            setBoard(board);
+        }
+        public int getPos(int x, int y)
+        {
+            return board[x, y];
+        }
+        public int[,] getBoard()
+        {
+            return board;
+        }
+        public bool tryMove(int x, int y, int newX, int newY)
+        {
+            if(board[x,y] != 0)
+            {
+                board[newX, newY] = board[x, y];
+                board[x, y] = 0;
+            }
+            return false;
+        }
+        public void setBoard(int[,] board)
+        {
+            //Row
+            for (int i = 0; i < 8; i++)
+            {
+                //Col
+                for (int u = 0; u < 8; u++)
+                {
+                    if (i == 1 || i == 6)
+                    {
+                        board[i, u] = 1;
+                    }
+                    if(i == 0 || i == 7)
+                    {
+                        setFrontRow(board, i);
+                    }
+                }
+            }
+        }
+        public void setFrontRow(int[,] board, int i)
+        {
+            board[i, 0] = 2;
+            board[i, 1] = 3;
+            board[i, 2] = 4;
+            if(i == 0)
+            {
+                board[i, 3] = 5;
+                board[i, 4] = 6;
+            }else
+            {
+                board[i, 3] = 6;
+                board[i, 4] = 5;
+            }
+            board[i, 5] = 4;
+            board[i, 6] = 3;
+            board[i, 7] = 2;
+        }
+
+        //Test methods
+
+        public void printBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                //Col
+                for (int u = 0; u < 8; u++)
+                {
+                    Console.Write(board[i, u]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ UC-Chess/Core.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace UC_Chess
+{
+    //Test Comment
+    public class Core : Game
+    {
+        int windowWidth = 512;
+        int windowHeight = 512;
+        int tileWidth = 512 / 8;
+        int tileHeight = 512 / 8;
+
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D tempTile;
+        Texture2D tempPieceB;
+        Texture2D tempPieceW;
+
+        Chess board;
+
+        MouseState lastMouse;
+        Vector2 curSelect;
+
+        public Core()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            this.IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = 512;
+            graphics.PreferredBackBufferHeight = 512;
+            graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            board = new Chess();
+            board.printBoard();
+            curSelect = new Vector2(-1, -1);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            tempTile = Content.Load<Texture2D>("tile");
+            tempPieceB = Content.Load <Texture2D>("blackPawn");
+            tempPieceW = Content.Load<Texture2D>("whitePawn");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                board.tryMove(1, 0, 3, 0);
+            }
+
+            if(Mouse.GetState().LeftButton == ButtonState.Pressed && lastMouse.LeftButton == ButtonState.Released)
+            {
+                //TODO: No hard coding window size
+                if(Mouse.GetState().Position.X > 0 && Mouse.GetState().Position.X < windowWidth)
+                {
+                    if (Mouse.GetState().Position.Y > 0 && Mouse.GetState().Position.Y < windowHeight)
+                    {
+                        int boardPosY = (int)Mouse.GetState().Position.X / tileWidth;
+                        int boardPosX = (int)Mouse.GetState().Position.Y / tileHeight;
+                        if (curSelect.X == -1)
+                        {
+                            curSelect.X = boardPosX;
+                            curSelect.Y = boardPosY;
+                            System.Console.WriteLine(curSelect.ToString());
+                        }
+                        else
+                        {
+                            board.tryMove((int)curSelect.X, (int)curSelect.Y, boardPosX, boardPosY);
+                            curSelect = new Vector2(-1, -1);
+                        }
+                    }
+                }
+                
+            }
+
+            lastMouse = Mouse.GetState();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //TODO: 
+            //Move to dedicated render class
+            //Support larger graphics sizes
+            //Make not terrible/more expandable
+            spriteBatch.Begin();
+            for(int i = 0; i < 8; i++)
+            {
+                for(int u = 0; u < 8; u++)
+                {
+                    if (i % 2 > 0 && u % 2 == 0)
+                    {
+                        spriteBatch.Draw(tempTile, new Rectangle(i * tileWidth, u * tileHeight, tileWidth, tileHeight), Color.Black);
+                    }
+                    else if (i % 2 == 0 && u % 2 > 0)
+                    {
+                        spriteBatch.Draw(tempTile, new Rectangle(i * tileWidth, u * tileHeight, tileWidth, tileHeight), Color.Black);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(tempTile, new Rectangle(i * tileWidth, u * tileHeight, tileWidth, tileHeight), Color.White);
+                    }
+
+                    if (board.getBoard()[u,i] == 1)
+                    {
+                        //spriteBatch.Draw(tempPieceB, new Rectangle(i * 32, u * 32, 32, 32), Color.White);
+                    }
+
+                    if (board.getBoard()[u, i] == 1)
+                    {
+                        spriteBatch.Draw(tempPieceW, new Rectangle(i * tileWidth, u * tileHeight, tileWidth, tileHeight), Color.White);
+                    }
+                }
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
